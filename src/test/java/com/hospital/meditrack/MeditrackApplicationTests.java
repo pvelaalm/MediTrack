2569c9f4f7d61defcbc072d3c1e7fd7e305fd71f
Pedro <pvelaalm@myuax.com>
2026-03-06 08:33:25 +0100

    Creacion y configuracion básica del proyecto

---
+++ src/test/java/com/hospital/meditrack/MeditrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.meditrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

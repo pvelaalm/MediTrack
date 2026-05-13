4493b7b23a28723a81c8890d2e0ae028ee8560eb
Pedro Velasco <pvelaalm@myuax.com>
2026-05-13 09:17:51 +0200

    fix: Ajuste en entidad TareaClinica

--- src/main/java/com/hospital/meditrack/model/entity/TareaClinica.java
+++ src/main/java/com/hospital/meditrack/model/entity/TareaClinica.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TareaClinica {
+public class  TareaClinica {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
